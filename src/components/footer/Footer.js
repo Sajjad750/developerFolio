e4ace6b6e8e30b05ce61c7bb394c99c09966b24e
Sajjad <56935657+Sajjad750@users.noreply.github.com>
2024-06-01 16:05:32 +0500

    Footer changes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,26 +1,24 @@
-import React, { useContext } from "react";
+import React, {useContext} from "react";
 import "./Footer.scss";
-import { Fade } from "react-reveal";
+import {Fade} from "react-reveal";
 import emoji from "react-easy-emoji";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
-    const { isDark } = useContext(StyleContext);
-    return ( <
-        Fade bottom duration = { 1000 }
-        distance = "5px" >
-        <
-        div className = "footer-div" >
-        <
-        p className = { isDark ? "dark-mode footer-text" : "footer-text" } > { emoji("Made with ❤️ by Sajjad") } <
-        /p> <
-        p className = { isDark ? "dark-mode footer-text" : "footer-text" } >
-        Theme by { " " } <
-        a href = "https://github.com/Sajjad750" >
-        developerFolio <
-        /a> < /
-        p > <
-        /div> < /
-        Fade >
-    );
+  const {isDark} = useContext(StyleContext);
+  return (
+    <Fade bottom duration={1000} distance="5px">
+      <div className="footer-div">
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          {emoji("Developed By ❤️ by Sajjad Ahmad ")}
+        </p>
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          Theme by{" "}
+          <a href="https://github.com/Sajjad750">
+            Sajjad Ahmad
+          </a>
+        </p>
+      </div>
+    </Fade>
+  );
 }
